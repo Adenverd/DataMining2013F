498a0f3d3dd4b35d50a8ecb2bc21e8d10f924fe5
Bekt <bekt17@gmail.com>
2013-09-02 00:58:42 -0500

    Finished  Filters

--- src/ml/Filter.java
+++ src/ml/Filter.java
@@ -3,6 +3,7 @@ package ml;
 import dataMining.ml.Matrix;
 
 import java.util.List;
+import java.util.Vector;
 
 public class Filter extends SupervisedLearner {
 
@@ -15,10 +16,22 @@ public class Filter extends SupervisedLearner {
         this.pLearner = pLearner;
         this.pTransform = pTransform;
         this.filterInputs = filterInputs;
+        this.buffer = new Vector<Double>();
     }
 
     @Override
     public void train(Matrix features, Matrix labels) {
+        if (features.getRows() != labels.getRows()) {
+            throw new MLException("Features and labels must have the same number of rows.");
+        }
+        if (filterInputs) {
+            Matrix matrix = trainAndTransform(features);
+            pLearner.train(matrix, labels);
+
+        } else {
+            Matrix matrix = trainAndTransform(labels);
+            pLearner.train(features, matrix);
+        }
     }
 
     @Override
@@ -30,5 +43,17 @@ public class Filter extends SupervisedLearner {
             pLearner.predict(in, buffer);
             pTransform.untransform(buffer, out);
         }
+    }
+
+    private Matrix trainAndTransform(Matrix data) {
+        pTransform.train(data);
+        Matrix matrix = new Matrix();
+        matrix.copyMetaData(pTransform.outputTemplate());
+        matrix.newRows(data.getRows());
+
+        for (int i = 0; i < data.getRows(); i++) {
+            pTransform.transform(data.row(i), matrix.row(i));
+        }
+        return matrix;
     }
 }
