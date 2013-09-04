0397943ae18902426335e6a4d580dc0a151a6cb5
Bekt <bekt17@gmail.com>
2013-09-03 20:32:55 -0500

    getAccuracy()

--- src/ml/SupervisedLearner.java
+++ src/ml/SupervisedLearner.java
@@ -12,7 +12,34 @@ public abstract class SupervisedLearner {
 
     public abstract List<Double> predict(List<Double> in);
 
-    public double getAccuracy() {
-        return Matrix.UNKNOWN_VALUE;
+    /**
+     * Uses Hamming distance for Categorical attributes, and means for Continuous
+     * @return Vector based SSE
+     */
+    public double getAccuracy(Matrix features, Matrix labels) {
+        double sum = 0;
+        for (int i = 0; i < labels.getNumRows(); i++) {
+            List<Double> result = predict(features.getRow(i));
+
+            if (labels.getNumCols() != result.size()) {
+                throw new MLException("Returned result size is different than number of columns in labels!");
+            }
+
+            double magnitude = 0;
+            for (int j = 0; j < labels.getNumCols(); j++) {
+
+                double actual = labels.getRow(i).get(j);
+                double predicted = result.get(j);
+
+                if (labels.isCategorical(j)) {
+                    magnitude += actual == predicted ? 1 : 0;
+                } else {
+                    double res = actual - predicted;
+                    magnitude += res * res;
+                }
+            }
+            sum += magnitude;
+        }
+        return sum;
     }
 }
